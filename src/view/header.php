7db9df571e7611551102ffe12a9e039e552c7b02
Yuri Reis <yuriazdreis@gmail.com>
2022-01-26 14:35:06 -0300

    FIX: Altera o caminho ao css e js

--- src/view/header.php
+++ src/view/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="http://localhost/wk-site/src/view/css/index.css">
-    <script src="http://localhost/wk-site/src/view/js/scripts.js"></script>
+    <link rel="stylesheet" href="http://localhost/wk-tech-challenge/src/view/css/index.css">
+    <script src="http://localhost/wk-tech-challenge/src/view/js/scripts.js"></script>
     <title>Document</title>
 </head>
 <body>
